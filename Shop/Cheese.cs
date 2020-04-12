957b37a383b85a571c7f5ec8dccfc46da76f7e23
sveta2309 <63541930+sveta2309@users.noreply.github.com>
2020-04-12 15:04:40 +0300

    Add files via upload
    Create na new class

---
+++ Shop/Cheese.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Cheese : Products
+    {
+        public Cheese()
+        {
+            this._price = 25;
+            this._endDate = 8;
+        }
+    
+    }
+}
