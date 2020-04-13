074bef5365b40f80e2d3936d96fc8813d17cf8a5
viunroman <63499624+viunroman@users.noreply.github.com>
2020-04-13 16:23:42 +0300

    Add files via upload
    New class Tomat

---
+++ Shop/Tomato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Tomato : Products
+    {
+        public Tomato()
+        {
+            this._price = 5;
+            this._endDate = 5;
+        }
+    
+    }
+}
