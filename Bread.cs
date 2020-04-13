37d0c8a45df6b563dbed92cf7978e439f604a208
sveta2309 <63541930+sveta2309@users.noreply.github.com>
2020-04-13 16:35:38 +0300

    Add files via upload

---
+++ Bread.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Bread:Products
+    {
+        public Bread()
+        {
+            this._price = 3;
+            this._endDate = 12;
+        }
+    }
+}
