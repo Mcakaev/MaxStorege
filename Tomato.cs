6528e6c3bf9cb950e1eb3ab419c83f980c6b085a
Mcakaev <63405150+Mcakaev@users.noreply.github.com>
2020-04-13 16:09:25 +0300

    Add files via upload
    add new class

---
+++ Tomato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class Tomato : Products
+    {
+        public Tomato()
+        {
+            this._price = 3;
+            this._endDate = 5;
+        }
+    
+    }
+}
